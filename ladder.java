471f54ca4222db9eebc29b31ebe622625472f4f1
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2020-12-05 10:30:03 +0530

    Added UC1

---
+++ ladder.java
@@ -0,0 +1,17 @@
+
+public class workshop {
+	int player1= 0;
+	int pos=0;
+	
+	int dicevalue = 0;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println(currentplayer==-1 "\nFirst player turn":);
+			System.out.println(getinitialplayer=0);
+		
+	}
+
+}

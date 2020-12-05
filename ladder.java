64962499375123302130133943ee6c961ea4a941
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2020-12-05 12:58:43 +0530

    Added UC1

--- ladder.java
+++ ladder.java
@@ -1,27 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
 
-public class workshop {
-	int player1= 0;
-	int pos=0;
+public class snake_and_ladder {
 	
-	int dicevalue = 0;
-
-
-public int rollDice()
+	public static int rollDice()
 	{
 	int n=0;
 	Random r = new Random();
-	n=r.nextIN=nt(7);
-	return (n==0?1:n)
+	n=r.nextInt(7);
+	return (n==0?1:n);
 	}
 	
-
-
+	int player1= 0;
+	int currentplayer=-1;
+	int pos=0;
+	static Scanner s =  new Scanner(System.in);
+	static String Str;
+	
+	static int dicevalue = 0;
+		
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-			System.out.println(currentplayer==-1 "\nFirst player turn":);
-			System.out.println(getinitialplayer=0);
-		
+			int initialplayer;
+			System.out.println(initialplayer=0);
+
+			
+				System.out.println("Press r to roll the dice");
+				Str = s.next();
+				dicevalue = rollDice();
+			
 	}
 
 }
